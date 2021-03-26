566ec14e8b9d277a9dd4ff3ae26dc693e49fe60a
Steve Ayers <californiastephen@outlook.com>
2021-03-26 10:51:07 -0700

    updated

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,5 @@
 from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import twitter_samples, stopwords
+# from nltk.corpus import twitter_samples, stopwords
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist, classify, NaiveBayesClassifier
